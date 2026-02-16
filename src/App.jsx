1b78afb8e7f99cc306ddac203cbdd64ce3f68ab9
Rajat <rajat804@gmail.com>
2026-02-16 17:09:59 +0530

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopBar from "./components/TopBar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import FloatingContact from "./components/FloatingContact";
+import Home from "./pages/Home";
+import GarbageBags from "./pages/GarbageBags";
+import KitchenTowel from "./pages/KitchenTowel";
+import BulkOrder from "./pages/BulkOrder";
+import AboutUs from "./pages/Aboutus";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
-
   return (
     <BrowserRouter>
+      <TopBar/>
+      <Header/>
       <Routes>
-          <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/garbage-bags" element={<GarbageBags />} />
+        <Route path="/kitchen-towel" element={<KitchenTowel/>}></Route>
+        <Route path="/bulk-order" element={<BulkOrder/>}></Route>
+        <Route path="/about" element={<AboutUs/>}></Route>
+        <Route path="/contact" element={<ContactPage/>}></Route>
+        {/* Catch-all route for 404 */}
+        <Route path="*" element={<h1 className="text-center mt-10">Page Not Found</h1>} />
       </Routes>
+      <Footer/>
+      <FloatingContact/>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
