9607c0433729f3fec55d165a7364eb4ec947b567
Rajat <rajat804@gmail.com>
2026-02-17 14:12:11 +0530

    dashboard admin

---
+++ src/pages/admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <div className="text-white p-10">
+      <h1 className="text-3xl font-bold">
+        Admin Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default Dashboard;
