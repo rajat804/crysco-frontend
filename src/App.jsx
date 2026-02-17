ed90a9b142742b29d6ed7ab4b2ab6a448dba7105
Rajat <rajat804@gmail.com>
2026-02-17 13:47:09 +0530

    app update jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import AboutUs from "./pages/AboutUs";
 import ContactPage from "./pages/ContactPage";
 import ProductDetails from "./pages/ProductDetails";
 import Customized from "./pages/Customized";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -26,6 +28,9 @@ function App() {
         <Route path="/contact" element={<ContactPage/>}></Route>
         <Route path="/product-details" element={<ProductDetails/>}></Route>
         <Route path="/customized" element={<Customized/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+
         {/* Catch-all route for 404 */}
         <Route path="*" element={<h1 className="text-center mt-10">Page Not Found</h1>} />
       </Routes>
