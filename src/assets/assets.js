a88c65ce382da5cb93192c868086865a8d779ab3
Rajat <rajat804@gmail.com>
2026-02-17 12:09:06 +0530

    assets js new page

---
+++ src/assets/assets.js
@@ -0,0 +1,45 @@
+import garbageImg from "./yello3.jpg";
+import kitchenTowelImg from "./3.jpg";
+import KitchenTissueImg from "./9.jpg";
+import bulkImg from "./6.jpg";
+import image1 from "./trikayaImg1.jpeg";
+import image2 from "./trikayaFactory.jpeg";
+import image3 from "./trikayaImg2.jpeg";
+import image4 from "./trikayaImg3.jpeg";
+import video1 from "./video1.mp4";
+import video2 from "./video2.mp4";
+import video3 from "./video3.mp4";
+import video4 from "./video4.mp4";
+import certificate from "./Picture1.png";
+import sampleImg1 from "./sampleImg1.jpeg";
+import sampleImg2 from "./sampleImg2.jpeg";
+import sampleImg3 from "./sampleImg3.jpeg";
+import sampleImg4 from "./sampleImg4.jpeg";
+import sampleImg5 from "./sampleImg5.jpeg";
+import sampleImg6 from "./sampleImg6.jpeg";
+import sampleVideo from "./sampleVideo1.mp4";
+
+
+
+export default {
+  garbageImg,
+  kitchenTowelImg,
+  KitchenTissueImg,
+  bulkImg,
+  image2,
+  image1,
+  image3,
+  image4,
+  video1,
+  video2,
+  video3,
+  video4,
+  certificate,
+  sampleImg1,
+  sampleImg2,
+  sampleImg3,
+  sampleImg4,
+  sampleImg5,
+  sampleImg6,
+  sampleVideo
+};
