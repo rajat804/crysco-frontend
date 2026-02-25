348a83c2d38bbc2d5c0faa2f169f8f76c2f962eb
Rajat <rajat804@gmail.com>
2026-02-25 15:51:27 +0530

    user layout

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -2,21 +2,20 @@ import { Outlet, useNavigate } from "react-router-dom";
 import UserSidebar from "../components/user/UserSidebar";
 import { useState } from "react";
 import { Menu } from "lucide-react";
+import { useAuth } from "../context/AuthContext";
 
 const UserLayout = () => {
+  const { logout } = useAuth();
   const [sidebarOpen, setSidebarOpen] = useState(false);
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-
-    navigate("/login");
+  const handleLogout = () => {
+    logout();
+    navigate("/login", { replace: true });
   };
 
   return (
     <div className="min-h-screen bg-slate-100">
-      
       {/* 🔹 Mobile Header */}
       <div className="lg:hidden flex items-center justify-between bg-white shadow p-4">
         <h1 className="text-lg font-bold">My Account</h1>
@@ -26,17 +25,15 @@ const navigate = useNavigate();
       </div>
 
       <div className="max-w-6xl mx-auto lg:p-8 p-4 grid lg:grid-cols-4 gap-6">
-        
         <UserSidebar
           sidebarOpen={sidebarOpen}
           setSidebarOpen={setSidebarOpen}
-           handleLogout={handleLogout}
+          handleLogout={handleLogout}
         />
 
         <div className="lg:col-span-3">
           <Outlet />
         </div>
-
       </div>
     </div>
   );
