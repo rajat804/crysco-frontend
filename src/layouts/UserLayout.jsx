f37e37678103e0e21b8a8a403f090c191a024433
Rajat <rajat804@gmail.com>
2026-02-25 12:12:12 +0530

    user layout

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,45 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import UserSidebar from "../components/user/UserSidebar";
+import { useState } from "react";
+import { Menu } from "lucide-react";
+
+const UserLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+const navigate = useNavigate();
+
+    const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    navigate("/login");
+  };
+
+  return (
+    <div className="min-h-screen bg-slate-100">
+      
+      {/* 🔹 Mobile Header */}
+      <div className="lg:hidden flex items-center justify-between bg-white shadow p-4">
+        <h1 className="text-lg font-bold">My Account</h1>
+        <button onClick={() => setSidebarOpen(true)}>
+          <Menu size={24} />
+        </button>
+      </div>
+
+      <div className="max-w-6xl mx-auto lg:p-8 p-4 grid lg:grid-cols-4 gap-6">
+        
+        <UserSidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+           handleLogout={handleLogout}
+        />
+
+        <div className="lg:col-span-3">
+          <Outlet />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default UserLayout;
