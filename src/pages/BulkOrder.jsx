ff7d6b57f450fd3001797facf923343e28d8f94c
Rajat <rajat804@gmail.com>
2026-03-02 18:22:25 +0530

    bulkorder update

--- src/pages/BulkOrder.jsx
+++ src/pages/BulkOrder.jsx
@@ -142,11 +142,11 @@ const BulkOrder = () => {
           Bulk Product Collection
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {bulkProducts.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
-        </div>
+        </div> */}
       </section>
 
       {/* ================= WHY CHOOSE US ================= */}
