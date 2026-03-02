98957bdc22a5f3c5ed0aa804324bcd550fc82f9b
Rajat <rajat804@gmail.com>
2026-03-02 18:22:46 +0530

    garbage bas

--- src/pages/GarbageBags.jsx
+++ src/pages/GarbageBags.jsx
@@ -54,8 +54,7 @@ const GarbageBags = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {/* Render ProductCard */}
           <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
+          
         </div>
       </section>
       </>
