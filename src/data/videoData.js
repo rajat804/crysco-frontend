a5a7a92242c70f07a440423b5bb4f266deff4964
Rajat <rajat804@gmail.com>
2026-03-06 16:06:06 +0530

    video data update

---
+++ src/data/videoData.js
@@ -0,0 +1,20 @@
+export const videoData = {
+  "kitchen tissue": [
+    
+  ],
+
+  "kitchen towel": [
+    
+  ],
+
+  "garbage bags": [
+    "/videos/garbage1.mp4",
+    "/videos/garbage2.mp4",
+    "/videos/garbage3.mp4",
+    "/videos/garbage4.mp4",
+    "/videos/garbage5.mp4",
+    "/videos/garbage6.mp4",
+    "/videos/garbage7.mp4",
+
+  ],
+};
