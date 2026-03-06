9eb04b95357d18c8b3dd44707ad3ca54a686e873
Rajat <rajat804@gmail.com>
2026-03-06 21:36:16 +0530

    home videos

---
+++ src/data/homeVideos.js
@@ -0,0 +1,22 @@
+export const homeVideos = {
+  "kitchen tissue": [
+    
+  ],
+
+  "kitchen towel": [
+    
+  ],
+
+  "garbage bags": [
+    "/videos/garbage1.mp4",
+    "/videos/garbage2.mp4",
+    "/videos/garbage3.mp4",
+    "/videos/garbage4.mp4",
+    "/videos/garbage5.mp4",
+    "/videos/garbage6.mp4",
+    "/videos/garbage7.mp4",
+
+  ],
+};
+
+export default homeVideos;
